c949169cb9ddbefbdf8ef4dff45cfb0417cd502a
AndroidPorts <androidports@users.noreply.github.com>
2017-12-15 11:00:58 +0900

    Add getgrgid_r stub.

--- grp.c
+++ grp.c
@@ -7,7 +7,13 @@ void endgrent(void) {
 }
 
 int getgrnam_r(const char *name, struct group *grp, char *buf, size_t buflen, struct group **result) {
-  fprintf(getgrnam_r, "endgrent: stub!!!");
+  fprintf(stderr, "getgrnam_r: stub!!!");
+  *result = NULL;
+  return -1;
+}
+
+int getgrgid_r(gid_t gid, struct group *grp, char *buf, size_t buflen, struct group **result) {
+  fprintf(stderr, "getgrnam_r: stub!!!");
   *result = NULL;
   return -1;
 }
